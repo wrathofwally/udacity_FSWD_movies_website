942c1f17b32a4d7981f69ffe7551cbfb6f26af1d
wrathofwally <walterparkesq@gmail.com>
2015-08-20 15:33:18 +1000

    Create media.py

---
+++ media.py
@@ -0,0 +1,6 @@
+class Movie():
+	def __init__(self, movie_title, movie_synopsis, movie_poster_image_url, movie_youtube_trailer_url):
+		self.title = movie_title
+		self.synopsis = movie_synopsis
+		self.poster_image_url = movie_poster_image_url
+		self.trailer_youtube_url = movie_youtube_trailer_url
